64dcae5633aad35b05aad9b4b24e3d49732d6c78
Teli75 <7telita@gmail.com>
2023-09-29 14:02:50 -0700

    fixed color and payment section

--- js/script.js
+++ js/script.js
@@ -15,8 +15,6 @@ jobRoleSelect.addEventListener("change", (e) => {
     otherJobRole.style.display = "block";
     otherJobRole.removeAttribute("hidden");
   } else {
-    //otherJobRole.style.visibility='hidden';
-    //otherJobRole.removeAttribute('style[display: block]');
     otherJobRole.style.display = "none";
     otherJobRole.hidden = "true";
     //otherJobRole.display='none'
@@ -28,17 +26,23 @@ const designSelectElement = document.querySelector("#design");
 const colorSelectElement = document.querySelector("#color");
 const colorOptions = colorSelectElement.children;
 
-colorSelectElement.disabled = "true";
+colorSelectElement.disabled = true;
 
 designSelectElement.addEventListener("change", (event) => {
   //colorSelectElement.disabled='false';
   colorSelectElement.removeAttribute("disabled");
+
+  
   for (let i = 1; i < colorOptions.length; i++) {
     const designTheme = event.target.value;
     const colorTheme = colorSelectElement[i].getAttribute("data-theme");
+    //<select id="color">
+    //<option selected hidden>Select a design theme above</option>
+
 
     //Displays colors available if theme matches
     if (colorTheme == designTheme) {
+      colorSelectElement[0].selected=true;
       colorSelectElement[i].removeAttribute("hidden");
     } else {
       colorSelectElement[i].hidden = true;
@@ -81,7 +85,6 @@ creditCard.style.display = "block";
 payPal.style.display = "none";
 bitcoin.style.display = "none";
 
-//paymentMethod.children.setAttribute('hidden', true);
 //displays payment method selected and hides unselected options
 paymentMethod.addEventListener("change", (e) => {
   const paymentSelection = e.target.value;
@@ -91,9 +94,14 @@ paymentMethod.addEventListener("change", (e) => {
     creditCard.style.display = "block";
     payPal.style.display = "none";
     bitcoin.style.display = "none";
-  } else {
+  } if(paymentSelection == "paypal") {
+    payPal.style.display = "block";
     creditCard.style.display = "none";
-    paymentMethod.style.display = "block";
+    bitcoin.style.display = "none";
+  } if(paymentSelection == "bitcoin") {
+    bitcoin.style.display = "block";
+    creditCard.style.display = "none";
+    paypal.style.display = "none";
   }
 });
 
@@ -105,58 +113,47 @@ const cvv = document.querySelector("#cvv");
 
 //validation of input fields
 form.addEventListener("submit", (event) => {
-  const nameValue = nameElement.value;
-  const emailValue = email.value;
-  const cardValue = cardNumber.value;
-  const zipValue = zipCode.value;
-  const cvvValue = cvv.value;
+ 
+  const nameIsValid = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameElement.value);
+  const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(email.value);
+  const cardIsValid = /^\d{13,16}$/.test(cardNumber.value);
+  const zipIsValid = /^\d{5}$/.test(zipCode.value);
+  const cvvIsValid = /^\d{3}$/.test(cvv.value);
 
-  const nameIsValid = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameValue);
-  const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailValue);
-  const cardIsValid = /^\d{13,16}$/.test(cardValue);
-  const zipIsValid = /^\d{5}$/.test(zipValue);
-  const cvvIsValid = /^\d{3}$/.test(cvvValue);
+  if(activities.checked == true){
 
-  //Calls visual validation function
-  visualValidation(nameIsValid, nameElement);
-  visualValidation(emailIsValid, email);
-  visualValidation(cardIsValid, cardNumber);
-  visualValidation(zipIsValid, zipCode);
-  visualValidation(cvvIsValid, cvv);
+  }
+  
 
   if(!nameIsValid || !emailIsValid || !cardIsValid || !zipIsValid || !cvvIsValid){
     event.preventDefault();
-  }
-
-  if (nameIsValid){
-    console.log('name is valid')
-  } if (emailIsValid){
-    console.log(`email is valid`);
-  } if (cardIsValid){
-    console.log(`card is valid`);
-  } if (zipIsValid){
-    console.log(`zip is valid`);
-  } if (cvvIsValid) {
-    console.log(`cvv is valid`);
   } else {
-    event.preventDefault();
+    
   }
-
-  
+   
+  //Calls visual validation function
+  visualValidation(nameIsValid, nameElement, 'name');
+  visualValidation(emailIsValid, email, 'email');
+  visualValidation(cardIsValid, cardNumber, 'cardNumber');
+  visualValidation(zipIsValid, zipCode, 'zipCode');
+  visualValidation(cvvIsValid, cvv, 'cvv');
+  //visualValidation(formIsValid, form);
 });
 
 // This function add or removes valid class depending on validation
-function visualValidation(elementIsValid, element) {
+function visualValidation(elementIsValid, element, string) {
   if (elementIsValid) {
     element.parentElement.classList.add('valid');
     element.parentElement.classList.remove('not-valid');
     element.parentElement.lastElementChild.style.display='none';
+    console.log(`${string} is valid`);
+   
   }
   if (!elementIsValid) {
-    element.parentElement.classList.add(".not-valid");
-    element.parentElement.classList.remove("valid");
+    element.parentElement.classList.add('not-valid');
+    element.parentElement.classList.remove('valid');
     element.parentElement.lastElementChild.style.display = "block";
-   
+    console.log(`${string} is NOT valid`);
   }
 }
 
