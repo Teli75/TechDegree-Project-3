1e34cd1e0de789c99b34de70c8eda550e44640d9
Teli75 <7telita@gmail.com>
2023-09-29 01:20:58 -0700

    finished visual validation

--- js/script.js
+++ js/script.js
@@ -1,31 +1,16 @@
-// Name Field
-// When the page first loads, the first text field should have the focus state by default to prompt the user.
-
-// Use the focus() method on the <input type="text"> element for the "Name" field.
-
+//Cursor on the name field when it first loads
 const nameElement = document.querySelector("#name");
 nameElement.focus();
 
-/*Job Role Section
-The "Job Role" section has an <input type="text"> field where users can enter a custom job role. If the user selects "Other" in the "Job Role" drop-down menu, they can enter info into the "Other job role" text field. This field should be hidden by default and only be displayed if "Other" is selected in the drop-down menu.
-
-Hide the "text field" with the id of "other-job-role" so it is not displayed when the form first loads.*/
-
-//const otherJobSelected = document.querySelector('option[value="Other"]');
-//Program the "Job Role" <select> element to listen for changes. When a change is detected, display/hide the "text field" based on the selection in the drop-down menu.
-
+//Job Role Section
 const jobRoleSelect = document.querySelector("#title");
 const otherJobRole = document.querySelector("#other-job-role");
 
 //hides "other job role" text box by default
 otherJobRole.hidden = "true";
 
-// console.log(jobRoleSelect);
-// console.log(otherJobRole);
-
+//if other is selected, text box is displayed
 jobRoleSelect.addEventListener("change", (e) => {
-  console.log(`you selected: ${e.target.value}`);
-
   if (e.target.value == "other") {
     otherJobRole.style.display = "block";
     otherJobRole.removeAttribute("hidden");
@@ -52,33 +37,29 @@ designSelectElement.addEventListener("change", (event) => {
     const designTheme = event.target.value;
     const colorTheme = colorSelectElement[i].getAttribute("data-theme");
 
-    console.log(`current design theme${i}: ${designTheme}`);
-    console.log(`current color theme${i}: ${colorTheme}`);
-
     //Displays colors available if theme matches
     if (colorTheme == designTheme) {
-      console.log(colorSelectElement[i]);
       colorSelectElement[i].removeAttribute("hidden");
     } else {
-      colorSelectElement[i].hidden = "true";
+      colorSelectElement[i].hidden = true;
     }
   }
 });
 
-const activities = document.querySelectorAll("#activities");
+const activities = document.querySelectorAll("#activities input");
 //why don't I need input after activities?"
 const total = document.querySelector("#activities-cost");
 let totalCost = 0;
 
 //activities.addEventListener("change", () => {
+//Adds cost of activity checked and provides a total
 document.querySelector(".activities").addEventListener("change", (e) => {
   const checkedActivity = e.target;
   const checkedActivityCost = +checkedActivity.getAttribute("data-cost");
 
-  console.log(activities);
 
   if (checkedActivity.checked) {
-    console.log(`checkedActivityCost ${checkedActivityCost}`);
+    //console.log(`checkedActivityCost ${checkedActivityCost}`);
     totalCost += checkedActivityCost;
     console.log(`Total: ${totalCost}`);
   } else {
@@ -87,107 +68,105 @@ document.querySelector(".activities").addEventListener("change", (e) => {
   total.innerHTML = `Total: ${totalCost}`;
 });
 
-//Program the "I'm going to pay with" <select> element to listen for user changes.
-
-//When a change is detected, hide all payment sections in the form’s UI except the selected one.
-
 const creditCard = document.querySelector(".credit-card");
-const payPal= document.querySelector(".paypal");
-const bitcoin= document.querySelector(".bitcoin");
+const payPal = document.querySelector(".paypal");
+const bitcoin = document.querySelector(".bitcoin");
 //why didn't the id work?
 
 //sets default to credit card selection
-const paymentMethod = document.querySelector('#payment');
-paymentMethod[1].selected = 'true';
+const paymentMethod = document.querySelector("#payment");
+paymentMethod[1].selected = "true";
 
-creditCard.style.display= 'block';
-payPal.style.display= 'none';
-bitcoin.style.display= 'none';
-//console.log(paymentMethod.children);
+creditCard.style.display = "block";
+payPal.style.display = "none";
+bitcoin.style.display = "none";
 
 //paymentMethod.children.setAttribute('hidden', true);
-
-
+//displays payment method selected and hides unselected options
 paymentMethod.addEventListener("change", (e) => {
   const paymentSelection = e.target.value;
   console.log(paymentSelection);
 
-  if (paymentSelection == 'credit-card') {
-    creditCard.style.display= 'block';
-    payPal.style.display= 'none';
-    bitcoin.style.display= 'none';
-  }else{
-    creditCard.style.display= 'none';
-    paymentMethod.style.display='block';
- 
+  if (paymentSelection == "credit-card") {
+    creditCard.style.display = "block";
+    payPal.style.display = "none";
+    bitcoin.style.display = "none";
+  } else {
+    creditCard.style.display = "none";
+    paymentMethod.style.display = "block";
   }
 });
 
+const form = document.querySelector("form");
+const email = document.querySelector("#email");
+const cardNumber = document.querySelector("#cc-num");
+const zipCode = document.querySelector("#zip");
+const cvv = document.querySelector("#cvv");
 
-const form = document.querySelector('form');
-const email = document.querySelector('#email');
-const cardNumber = document.querySelector('#cc-num');
-const zipCode = document.querySelector('#zip');
-const cvv = document.querySelector('#cvv');
-
-console.log(form);
-console.log(email);
-console.log(cardNumber);
-console.log(zipCode);
-console.log(cvv);
-
-form.addEventListener('submit', (event) =>{
+//validation of input fields
+form.addEventListener("submit", (event) => {
   const nameValue = nameElement.value;
   const emailValue = email.value;
   const cardValue = cardNumber.value;
   const zipValue = zipCode.value;
   const cvvValue = cvv.value;
 
-  // const nameValidator = () => {
-  //   const nameValue = nameElement.value;
-  //   console.log(`Name value is: ${nameValue}`);
-  //   // 2. Create a variable named `nameIsValid` to store the test value for this input.
+  const nameIsValid = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameValue);
+  const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailValue);
+  const cardIsValid = /^\d{13,16}$/.test(cardValue);
+  const zipIsValid = /^\d{5}$/.test(zipValue);
+  const cvvIsValid = /^\d{3}$/.test(cvvValue);
+
+  //Calls visual validation function
+  visualValidation(nameIsValid, nameElement);
+  visualValidation(emailIsValid, email);
+  visualValidation(cardIsValid, cardNumber);
+  visualValidation(zipIsValid, zipCode);
+  visualValidation(cvvIsValid, cvv);
+
+  if(!nameIsValid || !emailIsValid || !cardIsValid || !zipIsValid || !cvvIsValid){
+    event.preventDefault();
+  }
+
+  if (nameIsValid){
+    console.log('name is valid')
+  } if (emailIsValid){
+    console.log(`email is valid`);
+  } if (cardIsValid){
+    console.log(`card is valid`);
+  } if (zipIsValid){
+    console.log(`zip is valid`);
+  } if (cvvIsValid) {
+    console.log(`cvv is valid`);
+  } else {
+    event.preventDefault();
+  }
+
   
-       const nameIsValid = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameValue);
-       const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailValue);
-       const cardIsValid = /^\d{13,16}$/.test(cardValue);
-       const zipIsValid = /^\d{5}$/.test(zipValue);
-       const cvvIsValid = /^\d{3}$/.test(cvvValue);
-      
+});
 
+// This function add or removes valid class depending on validation
+function visualValidation(elementIsValid, element) {
+  if (elementIsValid) {
+    element.parentElement.classList.add('valid');
+    element.parentElement.classList.remove('not-valid');
+    element.parentElement.lastElementChild.style.display='none';
+  }
+  if (!elementIsValid) {
+    element.parentElement.classList.add(".not-valid");
+    element.parentElement.classList.remove("valid");
+    element.parentElement.lastElementChild.style.display = "block";
+   
+  }
+}
 
-      //  if (nameIsValid){
-      //   console.log(`name is valid`);
-      //     event.preventDefault();
-      //  } 
-      //  if (emailIsValid){
-      //   console.log(`email is valid`);
-      //     event.preventDefault();
-      //  } else {
-      //   event.preventDefault();
-      //  }
-      //  if (cardIsValid){
-      //   console.log(`card is valid`);
-      //     event.preventDefault();
-      //  } else {
-      //   event.preventDefault();
-      //  }
-      //  if (zipIsValid){
-      //   console.log(`zip is valid`);
-      //     event.preventDefault();
-      //  } else {
-      //   event.preventDefault();
-      //  }
-      //  if (cvvIsValid){
-      //   console.log(`cvv is valid`);
-      //     event.preventDefault();
-      //  } else {
-      //   event.preventDefault();
-      //  }
-        if (nameIsValid && emailIsValid && cardIsValid && zipIsValid && cvvIsValid){
-          console.log('do nothing');
-        } else {
-          preventDefault();
-        }
-  //event.preventDefault();
-})
+// Iterates through activities, so that when checked they trigger focus element on parent
+for (let i = 0; i < activities.length; i++) {
+  activities[i].addEventListener("focus", (e) => {
+    //const checkedActivity = e.target;
+    activities[i].parentElement.classList.add("focus");
+  });
+  activities[i].addEventListener("blur", (e) => {
+    activities[i].parentElement.classList.remove("focus");
+  });
+}
